6e04e3df18d630c5cf568dd9b05eb4beb97685d3
Bui Long <longbh.bof@cenhomes.vn>
2021-04-16 17:55:43 +0700

    new fix html

--- js/style.js
+++ js/style.js
@@ -2,18 +2,55 @@
 $(document).ready(function ()
 {
 
-    $('#tqSlider').owlCarousel({
-        items: 1,
+    $('#sliderTop').owlCarousel({ 
         loop: true,
-        margin: 0,
+        margin: 20,
         nav: true,
-        dots: true,
+        dots: false,
         navText: false,
         autoplay: false,
         navText: [
             '<i class="fas fa-chevron-left"></i>',
             '<i class="fas fa-chevron-right"></i>'
         ],
+        responsive: {
+            0: {
+                items: 2,
+            },
+            // breakpoint from 480 up
+            480: {
+                items: 3,
+            },
+            // breakpoint from 768 up
+            768: {
+                items: 5,
+            }
+        }
+    });
+    $('#sliderBottom').owlCarousel({ 
+        loop: true,
+        margin: 20,
+        nav: true,
+        dots: false,
+        navText: false,
+        autoplay: false,
+        navText: [
+            '<i class="fas fa-chevron-left"></i>',
+            '<i class="fas fa-chevron-right"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 2,
+            },
+            // breakpoint from 480 up
+            480: {
+                items: 3,
+            },
+            // breakpoint from 768 up
+            768: {
+                items: 5,
+            }
+        }
     });
 
     $('#ttSlider').owlCarousel({
