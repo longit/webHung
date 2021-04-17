b6211653b9228031ab7fd006cbe956bc173bb83f
Bui Long <builong83@gmail.com>
2021-04-17 15:26:40 +0700

    newcm

--- js/style.js
+++ js/style.js
@@ -1,7 +1,5 @@
 
-$(document).ready(function ()
-{
-
+$(document).ready(function (){ 
     $('#sliderTop').owlCarousel({ 
         loop: true,
         margin: 20,
@@ -51,196 +49,19 @@ $(document).ready(function ()
                 items: 5,
             }
         }
-    });
+    }); 
 
-    $('#ttSlider').owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 0,
-        nav: true,
-        dots: true,
-        navText: false,
-        autoplay: false,
-        navText: [
-            '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
-        ],
-    });
-    $('#tienichSlider').owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
-        dots: false,
-        navText: false,
-        autoplay: false,
-        navText: [
-            '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
-        ],
-        responsive: {
-            0: {
-                items: 1,
-            },
-            // breakpoint from 480 up
-            480: {
-                items: 1,
-            },
-            // breakpoint from 768 up
-            768: {
-                items: 2,
-            }
-        }
-    });
-    $('#chinhsachSlider').owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 0,
-        nav: true,
-        dots: true,
-        navText: false,
-        autoplay: false,
-        navText: [
-            '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
-        ],
-    });
-    $('#tabs1Slider').owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 0,
-        nav: true,
-        dots: false,
-        navText: false,
-        autoplay: false,
-        navText: [
-            '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
-        ],
-    });
-    $('#tabs3Slider').owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 0,
-        nav: true,
-        dots: false,
-        navText: false,
-        autoplay: false,
-        navText: [
-            '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
-        ],
-    });
-    $('#tabs4Slider').owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 0,
-        nav: true,
-        dots: false,
-        navText: false,
-        autoplay: false,
-        navText: [
-            '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
-        ],
-    });
-    $('#tabs5Slider').owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 0,
-        nav: true,
-        dots: false,
-        navText: false,
-        autoplay: false,
-        navText: [
-            '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
-        ],
-    });
-    $('#tabs6Slider').owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 0,
-        nav: true,
-        dots: false,
-        navText: false,
-        autoplay: false,
-        navText: [
-            '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
-        ],
-    });
-    $('#tabs7Slider').owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 0,
-        nav: true,
-        dots: false,
-        navText: false,
-        autoplay: false,
-        navText: [
-            '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
-        ],
-    });
-    $('#tabs8Slider').owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 0,
-        nav: true,
-        dots: false,
-        navText: false,
-        autoplay: false,
-        navText: [
-            '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
-        ],
-    });
-    $('#tabs9Slider').owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 0,
-        nav: true,
-        dots: false,
-        navText: false,
-        autoplay: false,
-        navText: [
-            '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
-        ],
-    });
-    $('#tabs10Slider').owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 0,
-        nav: true,
-        dots: false,
-        navText: false,
-        autoplay: false,
-        navText: [
-            '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
-        ],
-    });
-
-
-
-
-    $(window).scroll(function ()
-    {
-        if ($(this).scrollTop() > 400)
-        {
-            $('.b-header').addClass("b-fixed");
-        } else
-        {
-            $('.b-header').removeClass("b-fixed");
-        }
-    });
-    $('a', $('.b-header__menu-navbar ul li')).click(function ()
-    {
-        var id = $(this).attr('href');
-        $('html, body').animate({ scrollTop: $(id).offset().top - 100 }, 700);
-        return false;
-    });
+    // $(window).scroll(function ()
+    // {
+    //     if ($(this).scrollTop() > 400)
+    //     {
+    //         $('.b-header').addClass("b-fixed");
+    //     } else
+    //     {
+    //         $('.b-header').removeClass("b-fixed");
+    //     }
+    // });
+     
 
     $(".b-header__menu-navbar ul li a").click(function ()
     {
